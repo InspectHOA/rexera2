d2e0969f4e3bc20d33be443217fe6feba1d08259
sasidhar <janakisasidhar1@gmail.com>
2025-07-24 15:30:21 +0530

    update

---
+++ serverless-api/api/workflows/[id]/counterparties/[relationshipId].ts
@@ -0,0 +1,2 @@
+// Export all HTTP method handlers for Vercel - handles /api/workflows/:id/counterparties/:relationshipId
+export { GET, POST, PUT, PATCH, DELETE, OPTIONS } from '../../../../src/app';
